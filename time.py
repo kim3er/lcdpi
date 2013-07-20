24f830683bdcefdb62a2ea81e98089b38d335c40
Richard Kimber <richard@dogma.co.uk>
2013-07-20 22:45:26 +0100

    Changed message

--- time.py
+++ time.py
@@ -10,5 +10,5 @@ my_lcd = lcd.Lcd()
 if __name__ == "__main__":
 	while True:
 		my_lcd.write_line(datetime.now().strftime('%T'), lcd.LCD_LINE_2, lcd.center)
-		my_lcd.write_line('Oliver smells', lcd.LCD_LINE_3, lcd.center)
+		my_lcd.write_line('Daddy Rocks!!!', lcd.LCD_LINE_3, lcd.center)
 		time.sleep(60)
