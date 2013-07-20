0f6abbc2335c5303fffc393e75f0780a39215941
Richard Kimber <richard@dogma.co.uk>
2013-07-20 20:04:03 +0100

    Added program start

--- time.py
+++ time.py
@@ -7,7 +7,8 @@ from datetime import datetime
 
 my_lcd = lcd.Lcd()
 
-while True:
-	my_lcd.write_line(datetime.now().strftime('%T'), lcd.LCD_LINE_2, lcd.center)
-	my_lcd.write_line('Oliver smells', lcd.LCD_LINE_3, lcd.center)
-	time.sleep(60)
+if __name__ == "__main__":
+	while True:
+		my_lcd.write_line(datetime.now().strftime('%T'), lcd.LCD_LINE_2, lcd.center)
+		my_lcd.write_line('Oliver smells', lcd.LCD_LINE_3, lcd.center)
+		time.sleep(60)
